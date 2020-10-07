70e9c24e1a9537c56a810e3b370f0c920b55a498
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-10-07 17:11:16 +0700

    Add files via upload

---
+++ tugas_1_1.php
@@ -0,0 +1,79 @@
+<?php
+echo "<h1>Daftar Menu</h1>";
+
+echo "<table> 
+<tr>
+<td><li></td>
+<td>Ayama Goreng</td>
+<td>=</td>
+<td>Rp.</td>
+<td>13000</td>
+</tr>
+
+<tr>
+<td><li></td>
+<td>Ayama Bakar</td>
+<td>=</td>
+<td>Rp.</td>
+<td>15000</td>
+</tr>
+
+<tr>
+<td><li></td>
+<td>Ayama Sayur</td>
+<td>=</td>
+<td>Rp.</td>
+<td>13000</td>
+</tr>
+
+<tr>
+<td><li></td>
+<td>Tempe Goreng</td>
+<td>=</td>
+<td>Rp.</td>
+<td>1000</td>
+</tr>
+
+<tr>
+<td><li></td>
+<td>Tahu Goreng</td>
+<td>=</td>
+<td>Rp.</td>
+<td>1000</td>
+</tr>
+
+<tr>
+<td><li></td>
+<td>Soto</td>
+<td>=</td>
+<td>Rp.</td>
+<td>3000</td>
+</tr>
+
+<tr>
+<td><li></td>
+<td>Nasi Putih</td>
+<td>=</td>
+<td>Rp.</td>
+<td>5000</td>
+</tr>
+
+<tr>
+<td><li></td>
+<td>Es Teh</td>
+<td>=</td>
+<td>Rp.</td>
+<td>4000</td>
+</tr>
+
+<tr>
+<td><li></td>
+<td>Es Tebu</td>
+<td>=</td>
+<td>Rp.</td>
+<td>5000</td>
+</tr>
+
+</table>"
+?>
+
